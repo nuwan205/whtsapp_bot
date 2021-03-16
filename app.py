72fcd8fb43ce4f78405f8c9422cb6161f0b72a9f
nuwan205 <56704759+nuwan205@users.noreply.github.com>
2021-03-16 12:02:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,11 +39,11 @@ def bot():
             responded = True
 
     if 'menu' in incoming_msg.lower():
-        text = f'[+]BC4T (Nuwan Konara) \n\n*Menu :*  \n\n*YOU* _<url>_ : Youtube Downloader\n\n *FB* _<url>_ : Facebook Downloader \n\n *TRS* _<Text>_ : Translate to English to Sinhala\n\n *TRE* _<Text>_ : Translate Sinhala To English\n\n*pypdf*: Python Pdf set\n\n*dow* _<link>_ : download media items\n\n*cal* _<expression>_ : Calculator'
+        text = f'[+]BC4T (Nuwan Konara) \n\n*Menu :*  \n\n<url> : Youtube Downloader\n\n <url> : Facebook Downloader \n\n *TRS* _<Text>_ : Translate to English to Sinhala\n\n *TRE* _<Text>_ : Translate Sinhala To English\n\n*pypdf*: Python Pdf set\n\n*dow* _<link>_ : download media items\n\n*cal* _<expression>_ : Calculator'
         msg.body(text)
         responded = True
 
-    if 'fb' in incoming_msg.lower():
+    if 'https://www.facebook.com' in incoming_msg.lower():
         import requests as r
         import re
         par = incoming_msg[2:]
@@ -51,10 +51,10 @@ def bot():
         video_url = re.search('sd_src:"(.+?)"', html.text).group(1)
         msg.body(video_url)
         responded = True
-    if 'YOU' in incoming_msg.upper():
+    if 'https://youtu.be' in incoming_msg.lower():
         
         from pytube import YouTube
-        link = incoming_msg[3:]
+        link = incoming_msg
         yt = YouTube(link)
         print("Title: ",yt.title)
         print("Length of video: ",yt.length)
