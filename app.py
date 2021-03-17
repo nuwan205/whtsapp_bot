d7ef9863cba9c141e662cdc5ba41e120a3e03607
nuwan205 <56704759+nuwan205@users.noreply.github.com>
2021-03-17 17:11:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def bot():
         x=incoming_msg[3:]
         msg.media(x)
     if '$test' in incoming_msg:
-        msg.body("https://mp3fy.com/yr/Dl93t0FBxUs/video")
+        msg.body("https://cdn03.vid-loader.com/download/Dl93t0FBxUs/mp4/18/1615984826/aeb57ae9c102a8695cadd434a916b8b147105462390d1352105c370098185315/1")
     
         
       
