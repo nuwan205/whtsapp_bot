73cb010bb8ebca3993ae93eaa8e4c07c1fd63ca9
nuwan205 <56704759+nuwan205@users.noreply.github.com>
2021-03-20 18:33:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -119,18 +119,18 @@ def bot():
         msg.media(x)
     if '$tests' in incoming_msg:
         from selenium import webdriver
-        import os
-
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--no-sandbox")
-        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+        GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+        CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+        chrome_options = webdriver.ChromeOptions()chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')chrome_options.binary_location = GOOGLE_CHROME_PATH
+        
+        browser = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 
 
-        driver.get("https://www.google.com")
-        print(driver.page_source)
+
+
+        browser.get("https://www.google.com")
+        print(browser.page_source)
     if '/ya' in incoming_msg.lower():
         import requests
         msgs=incoming_msg[3:].split("/")[-1]
