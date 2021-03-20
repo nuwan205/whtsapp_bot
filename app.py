12a1455335ffa361bd56e309fd05b682bbd74970
nuwan205 <56704759+nuwan205@users.noreply.github.com>
2021-03-20 19:26:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -121,8 +121,10 @@ def bot():
         from selenium import webdriver
         GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
         CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
-        chrome_options = webdriver.ChromeOptions()chrome_options.add_argument('--disable-gpu')
-        chrome_options.add_argument('--no-sandbox')chrome_options.binary_location = GOOGLE_CHROME_PATH
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.binary_location = GOOGLE_CHROME_PATH
         
         browser = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
         msg.body("ok")
