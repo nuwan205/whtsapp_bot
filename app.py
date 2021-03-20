3895d19b43af3628922a33e29e70febe29f0ce18
nuwan205 <56704759+nuwan205@users.noreply.github.com>
2021-03-20 18:55:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ def bot():
 
         browser.get("https://www.google.com")
         print(browser.page_source)
-    if "11" in incoming_msg:
+    if "111" in incoming_msg:
         msg.body("ok")
     if '/ya' in incoming_msg.lower():
         import requests
