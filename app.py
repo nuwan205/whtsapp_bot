eec39f29bb105c56ad99a3589e17be9f1b1e7dff
nuwan205 <56704759+nuwan205@users.noreply.github.com>
2021-05-08 14:10:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def bot():
 
     responded = True
   
-    msg.body(incoming_msg)
+    print(incoming_msg)
     if '/st' in incoming_msg.lower():
         text = "Helo how are you:::::Type /menu for more"
         print(dir(msg))
